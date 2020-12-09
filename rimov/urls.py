ae34b250f4e2e61cfa154ba5afc6c5be0da2f908
EmmanuelVeiga <emmanuelveiga@outlook.com>
2020-12-09 19:20:24 -0400

    Atualização Front

--- rimov/urls.py
+++ rimov/urls.py
@@ -8,23 +8,25 @@ from system import views
 
 urlpatterns = [
 
+	path('accounts/', include('django.contrib.auth.urls')),
 	path('admin/', admin.site.urls),
 	path('', TemplateView.as_view(template_name="index.html")),
-	path('imoveis/', TemplateView.as_view(template_name="imoveis.html")),
 	path('painel/', TemplateView.as_view(template_name="painel.html")),
-	path('accounts/', include('django.contrib.auth.urls')),
+	path('painel/imoveis/', TemplateView.as_view(template_name="imoveis.html")),	
+	path('painel/google_maps/', TemplateView.as_view(template_name="google_maps.html")),
+	
 
     
-	path('lista_corretores/', views.lista_corretores, name='lista_corretores'),
-	path('criar_corretor/', views.createCorretor, name="criar_corretor"),
+	path('painel/lista_corretores/', views.lista_corretores, name='lista_corretores'),
+	path('painel/criar_corretor/', views.createCorretor, name="criar_corretor"),
 	path('visualizar_corretor/<str:id>', views.visualizarCorretor, name="visualizar_corretor"),
 	path('editar_corretor/<str:id>', views.editarCorretor, name='editar_corretor'),	
 	path('deletar_corretor/<str:id>/', views.deleteCorretor),
 
 
 
-	path('lista_imoveis/', views.lista_imoveis, name='lista_imoveis'),
-	path('criar_imovel/', views.createImovel, name="criar_imovel"),
+	path('painel/lista_imoveis/', views.lista_imoveis, name='lista_imoveis'),
+	path('painel/criar_imovel/', views.createImovel, name="criar_imovel"),
 	path('visualizar_imovel/<str:id>', views.visualizarImovel, name="visualizar_imovel"),
 	path('editar_imovel/<str:id>', views.editarImovel, name="editar_imovel"),	
 	path('deletar_imovel/<str:id>/', views.deleteImovel),
